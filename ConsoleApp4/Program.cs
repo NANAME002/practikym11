67f0db4ab154bda8b81d92675efbd8807db4d8f9
NANAME002 <dennzerogo@gmail.com>
2024-11-29 14:05:32 +0200

    add 4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Input("a= ");
+            int b = Input("b= ");
+            int c = Input("c= ");
+            int d = Input("d= ");
+            Console.WriteLine($"s={Max(a,b)+Max(c,d)}");
+            Console.ReadLine();
+
+        }
+        public static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        public static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
