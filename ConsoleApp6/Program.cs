aba6bb90ef5f27ca3616d111f81e51c965f6ef0a
NANAME002 <dennzerogo@gmail.com>
2024-11-29 14:39:06 +0200

    add 6

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = Input("r= ");
+            int k = Input("k= ");
+            int rez= Kop(r, k);
+            Output(rez);
+            Console.ReadLine();
+        }
+        public static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        public static int Kop(int r,int k)
+        {
+            return r * 100 + k;
+        }
+        public static void Output(int rez)
+        {
+            Console.WriteLine($"rez={rez}");
+        }
+    }
+}
