949b6b312c7f542a005fe31d2d1083b377125c2d
NANAME002 <dennzerogo@gmail.com>
2024-11-29 13:25:27 +0200

    add1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = Input("K= ");
+            int m = Input("m= ");
+            int rez =Meters(k, m);
+            Output(rez);
+            Console.ReadLine();
+        }
+        public static int Meters(int k,int m)
+        {
+            return k * 1000 + m;
+        }
+        public static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        public static void Output(int rez)
+        {
+            Console.WriteLine($"rez={rez}");
+        }
+    }
+}
